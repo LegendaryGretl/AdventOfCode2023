d2373aaf15fe88569046ceb3fed6b6790391ce53
LegendaryGretl <maansilv@ucsc.edu>
2023-12-12 19:51:18 -0800

    trying to write recursive method, but it's getting out of hand

--- d12/spring_has_sprung_a_leak.py
+++ d12/spring_has_sprung_a_leak.py
@@ -13,8 +13,39 @@ def correct_arrangement(condition, grouping):
         return False
     return True
 
+def recurse_arrange(sequence, pattern, sum=0):
+    if len(sequence) == 0:
+        if len(pattern) == 0:
+            return sum + 1
+        else:
+            return sum + 0
+    if len(pattern) == 0:
+        if sequence.find("#") != -1:
+            return sum + 0
+        else:
+            return sum + 1
+    if sequence[0] == ".":
+        return recurse_arrange(sequence[1:], pattern, sum)
+    if sequence[0] == "#":
+        if len(sequence) < pattern[0]:
+            return sum
+        if sequence[:pattern[0]].find(".") == -1:
+            if (len(sequence) == pattern[0] )or (sequence[pattern[0]] != "#"):
+                return recurse_arrange(sequence[pattern[0]:], pattern[1:], sum)
+            else:
+                return sum
+        else:
+            return sum + 0
+    if sequence[0] == "?":
+        return sum + recurse_arrange("."+sequence[1:], pattern, 0) + recurse_arrange("#"+sequence[1:], pattern, 0)
+    
+
 springs_txt = open("input.txt")
 springs_list = springs_txt.read().strip().split("\n")
 springs_list = [item.split() for item in springs_list]
 for i in range(len(springs_list)):
     springs_list[i][1] = (int(springs_list[i][1].split(",")[0]), int(springs_list[i][1].split(",")[1]))
+
+print(recurse_arrange("#.#.###", [1,1,3], 0))
+
+print(recurse_arrange(".??..??...?##.", [1,1,3], 0))
